901da418f1d34940c2f403d352f9ff6e787b4911
Ardiy0011 <ardiyquarsh@gmail.com>
2023-10-27 00:24:56 +0000

    get and count functions

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,19 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Retrieve an object based on class and ID"""
+        if cls and id in DBStorage.classes:
+            object = f"{cls.__name__}.{id}"
+            return self.__session.query(cls).get(object)
+        return None
+
+
+    def count(self, cls=None):
+        """Count the number of objects in storage"""
+        count = 0
+        for objectname in classes:
+            if cls == classes[objectname] or cls == None :
+                count += self.__session.query(classes[objectname]).count()
+        return count
